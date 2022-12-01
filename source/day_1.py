5b40b2650440fc7f60d8daf79d189f0fc627c25b
Achxy <luminosityzzz00@gmail.com>
2022-12-02 02:49:49 +0530

    a bit faster

--- source/day_1.py
+++ source/day_1.py
@@ -5,10 +5,11 @@ from advent import AdventSolution
 
 class Solution(AdventSolution, day=1):
     def __init__(self, data: str) -> None:
-        self.calories = [sum(map(int, chunk.split())) for chunk in data.split("\n\n")]
+        self.large = nlargest(3, [sum(map(int, chunk.split())) for chunk in data.split("\n\n")])
 
     def part_1(self):
-        return max(self.calories)
+        largest = self.large[0]
+        return largest
 
     def part_2(self):
-        return sum(nlargest(3, self.calories))
+        return sum(self.large)
