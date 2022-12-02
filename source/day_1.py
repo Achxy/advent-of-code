16822fd15291261a9785f8c924ef621f8aeb2df9
Achxy <luminosityzzz00@gmail.com>
2022-12-02 21:02:06 +0530

    slightly more efficient

--- source/day_1.py
+++ source/day_1.py
@@ -5,11 +5,10 @@ from advent import AdventSolution
 
 class Solution(AdventSolution, day=1):
     def __init__(self, data: str) -> None:
-        self.large = nlargest(3, [sum(map(int, chunk.split())) for chunk in data.split("\n\n")])
+        self.max, *self.top = nlargest(3, [sum(map(int, chunk.split())) for chunk in data.split("\n\n")])
 
     def part_1(self):
-        largest = self.large[0]
-        return largest
+        return self.max
 
     def part_2(self):
-        return sum(self.large)
+        return self.max + sum(self.top)
