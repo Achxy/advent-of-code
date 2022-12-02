f20de272b29e90b715200e846ecdaf8af775204b
Achxy <luminosityzzz00@gmail.com>
2022-12-02 23:12:12 +0530

    might as well call it a day

--- source/day_2.py
+++ source/day_2.py
@@ -2,23 +2,13 @@ from advent import AdventSolution
 
 
 class Solution(AdventSolution, day=2):
-    # TODO: There is a pattern here!!!
-    # Do complement wrapping so that the index is referenced
-    # will do it later :-)
     PLAY = {"A": ["Z", "X", "Y"], "B": ["X", "Y", "Z"], "C": ["Y", "Z", "X"]}
 
     def __init__(self, data: str) -> None:
         self.data = [line.split() for line in data.splitlines()]
 
     def part_1(self):
-        score = 0
-        for opponent, team in self.data:
-            score += self.PLAY[opponent].index(team) * 3 + ord(team) - 87
-        return score
+        return sum(self.PLAY[oppo].index(me) * 3 + ord(me) - 87 for oppo, me in self.data)
 
     def part_2(self):
-        score = 0
-        for opponent, result in self.data:
-            fate = ord(result) - 88
-            score += (fate * 3) + ord(self.PLAY[opponent][fate]) - 87
-        return score
+        return sum((fate := ord(res) - 88) * 3 + ord(self.PLAY[oppo][fate]) - 87 for oppo, res in self.data)
