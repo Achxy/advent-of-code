fec2788221ce3d0902ee038c20cffbdf265ba2be
Achxy <luminosityzzz00@gmail.com>
2022-12-02 22:26:44 +0530

    part 2 (with todos)

--- source/day_2.py
+++ source/day_2.py
@@ -1,8 +1,10 @@
 from advent import AdventSolution
 
 
-class Solution(AdventSolution, day=2, run=True):
+class Solution(AdventSolution, day=2):
     # TODO: There is a pattern here!!!
+    # Do complement wrapping so that the index is referenced
+    # will do it later :-)
     PLAY = {"A": ["Z", "X", "Y"], "B": ["X", "Y", "Z"], "C": ["Y", "Z", "X"]}
 
     def __init__(self, data: str) -> None:
@@ -11,5 +13,12 @@ class Solution(AdventSolution, day=2, run=True):
     def part_1(self):
         score = 0
         for opponent, team in self.data:
-            score += (self.PLAY[opponent].index(team) * 3) + (ord(team) - 87)
+            score += self.PLAY[opponent].index(team) * 3 + ord(team) - 87
+        return score
+
+    def part_2(self):
+        score = 0
+        for opponent, result in self.data:
+            fate = ord(result) - 88
+            score += (fate * 3) + ord(self.PLAY[opponent][fate]) - 87
         return score
