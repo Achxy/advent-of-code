bed47627e552970139c0576c4ddecc0649e0658f
Achxy <luminosityzzz00@gmail.com>
2022-12-03 13:43:23 +0530

    mul takes precedence

--- source/day_3.py
+++ source/day_3.py
@@ -10,7 +10,7 @@ class Solution(AdventSolution, day=3):
         return sum(self.get_priority(x) for x in arragement)
 
     def part_2(self):
-        arrangement = (map(set, m) for m in zip(*([iter(self.data)] * 3), strict=True))
+        arrangement = (map(set, m) for m in zip(*[iter(self.data)] * 3, strict=True))
         return sum(self.get_priority((x & y & z).pop()) for x, y, z in arrangement)
 
     @staticmethod
