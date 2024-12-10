543401c4fa49158caa5654ea91786856cf1b2349
Achyuth Jayadevan <achyuth@jayadevan.in>
2024-12-11 01:40:26 +0530

    golfed code

--- source/2024/day_10.py
+++ source/2024/day_10.py
@@ -6,31 +6,14 @@ class Solution(Advent, year=2024, day=10):
         self.data = [[int(p) for p in q] for q in data.splitlines()]
         self.max_x = len(self.data[0])
         self.max_y = len(self.data)
-        assert all(len(n) for n in self.data)
 
     def part_1(self, remember=True):
-        score = 0
-        for y in range(self.max_y):
-            for x in range(self.max_x):
-                if not self.data[y][x]:
-                    score += self._embark(x, y, 0, (x, y), set(), remember)
-        return score
+        return sum(self._embark(x, y, 0, (x, y), set(), remember) for y in range(self.max_y) for x in range(self.max_x) if not self.data[y][x])
 
     def part_2(self):
         return self.part_1(remember=False)
 
-    def _embark(self, x: int, y: int, value: int, origin, visited, remember) -> int:
-        if value == 9 and (k := (*origin, x, y)) not in visited:
-            if remember:
-                visited.add(k)
-            return 1
-        vp1, score = value + 1, 0
-        if x and self.data[y][x - 1] == vp1:
-            score += self._embark(x - 1, y, vp1, origin, visited, remember)
-        if y and self.data[y - 1][x] == vp1:
-            score += self._embark(x, y - 1, vp1, origin, visited, remember)
-        if self.max_x > (x + 1) and self.data[y][x + 1] == vp1:
-            score += self._embark(x + 1, y, vp1, origin, visited, remember)
-        if self.max_y > (y + 1) and self.data[y + 1][x] == vp1:
-            score += self._embark(x, y + 1, vp1, origin, visited, remember)
-        return score
+    def _embark(self, x, y, val, ori, vi, rem):
+        if val == 9 and (k := (*ori, x, y)) not in vi:
+            return (vi.add(k) if rem else None) or 1
+        return sum(self._embark(nx, ny, val + 1, ori, vi, rem) for nx, ny in [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)] if 0 <= nx < self.max_x and 0 <= ny < self.max_y and self.data[ny][nx] == val + 1)
